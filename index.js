c618f016705c90bbbd908d740de62c774a992e64
Sarang Joshi <sarangj@msn.com>
2016-10-27 03:16:16 -0700

    Starting Firebase Server SDK integration

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+var firebase = require("firebase");
+firebase.initializeApp({
+  serviceAccount: "uwcalendar-69fc31da40b7.json",
+  databaseURL: "https://uwcalendar.firebaseio.com"
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -16,5 +22,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
