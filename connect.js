15898b310ee6f16a15f0faf4081e1f9b74abaf20
Sarang Joshi <sarangj@msn.com>
2017-03-24 15:39:57 -0700

    Connect

--- connect.js
+++ connect.js
@@ -3,7 +3,7 @@
 // TODO: make ES6-y
 
 var _ = require('lodash');
-var FbConstants = require('./constants.js');
+var Constants = require('./constants.js');
 
 var NUMBER_OF_DAYS = 5;
 var LATEST_TIME = '23:59';
@@ -12,6 +12,7 @@ var NO_CLASS = 'NO_CLASS';
 
 /**
 * Given a quarter, returns a week representation.
+* A week is a sorted list of days. A day is a list of objects { time, currentClass }.
 */
 function getWeek(quarter) {
   var week = [];
@@ -40,34 +41,45 @@ function getWeek(quarter) {
 }
 
 /**
-* @param {list} days a list of days to combine
+* @param {object} days a map of user ID's to days combine
 * Combines days.
 */
 function combineDays(days) {
+  console.log("Days in combineDays:");
+  console.log(days);
+
   // Each day should be well-formed.
   // - at least one class
   // - start and end of each class --> length divisible by 2
-  if (days.values.some(function(day) { return !day.length || (day.length % 2); })) {
+  if (_.values(days).some(function(day) { return !day.length || (day.length % 2); })) {
     return null;
   }
 
   var combinedDay = [];
 
   //////// START ALGORITHM
-  var indices = _.mapValues(days, () => 0); // which marker we are considering for each day
-  var classes = _.mapValues(days, () => NO_CLASS); // the current set of classes that are active for the current time slot
+  var indices = _.mapValues(days, () => 0); // which marker we are considering for each user ID
+  var classes = _.mapValues(days, () => NO_CLASS); // the current set of classes, for each user ID, that are active for the current time slot
   var validIndices = _.mapValues(days, () => true); // keeps track of which users still have markers left to consider; starts out as all true's
 
-  // Go as long as there is at least 1 index that is valid
-  while (validIndices) {
+  while (true) {
+    // Go as long as there is at least 1 index that is valid
+    var allDone = false;
+    _.forOwn(validIndices, function(validIndex) { allDone = allDone || validIndex; });
+    if (!allDone) {
+      break;
+    }
+
     // Find which markers of the current ones are the earliest so far
     var earliestMarkers = [];
     var earliestTime = LATEST_TIME;
     _.forOwn(days, (day, userId) => {
-      if (validIndices & 1 << userId) {
+      // First check if we're considering this userId
+      if (validIndices[userId]) {
         var currMarker = day[indices[userId]];
 
         if (currMarker.time < earliestTime) {
+          // Reset the earliest markers, we found a new earliest user id!
           earliestMarkers = [userId];
           earliestTime = currMarker.time;
         } else if (currMarker.time == earliestTime) {
@@ -77,10 +89,13 @@ function combineDays(days) {
     });
 
     // Set the corresponding classes in `classes` to be the classes contained in each marker
-    // Move the current indices for the days just updated
-    earliestMarkers.forEach(function(dayIndex) {
-      classes[dayIndex] = days[dayIndex][indices[dayIndex]].currentClass;
-      indices[dayIndex]++;
+    earliestMarkers.forEach(function(userId) {
+      // Get the day for this user
+      var userDay = days[userId];
+      // Set that user's class to be its `currentClass`
+      classes[userId] = userDay[indices[userId]].currentClass;
+      // Move the current indices for the days just updated
+      indices[userId]++;
     });
 
     // Push a new marker with `classes` and the time of the marker
@@ -89,9 +104,9 @@ function combineDays(days) {
     });
 
     // Update the value of validIndices
-    indices.forEach(function(markerIndex, i) {
-      if (markerIndex >= days[i].length && (validIndices & 1 << i)) {
-        validIndices -= (1 << i);
+    _.forOwn(indices, function(markerIndex, userId) {
+      if (markerIndex >= days[userId].length && validIndices[userId]) {
+        validIndices[userId] = false;
       }
     });
 
@@ -107,16 +122,21 @@ function combineDays(days) {
 * @param {Snapshot} quarterA the Firebase snapshot for quarterA
 * @param {Snapshot} quarterB the Firebase snapshot for quarterB
 */
-function combineQuartersIntoWeek(quarterA, quarterB) {
+function combineQuartersIntoWeek(quarterA, quarterB, userA, userB) {
   // Now that the quarters have been retrieved, sanitize and combine them
+
+  // We don't care about the key for the class, so just store a list of the classes
   quarterA = _.values(quarterA);
   quarterB = _.values(quarterB);
 
   var weekA = getWeek(quarterA);
   var weekB = getWeek(quarterB);
 
-  var combinedWeek = weekA.map(function(dayOfWeek, i) {
-    return combineDays([dayOfWeek, weekB[i]]);
+  var combinedWeek = weekA.map(function(dayWeekA, i) {
+    return combineDays({
+      userA: dayWeekA,
+      userB: weekB[i]
+    });
   });
 
   console.log(JSON.stringify(combinedWeek));
@@ -131,25 +151,25 @@ function saveConnection(db, combinedWeek, quarter, userA, userB) {
   // Creating a promise
   return new Promise(function (resolve, reject) {
     // Add to connections collection
-    var connRef = db.ref(CONNECTIONS_KEY).push();
+    var connRef = db.ref(Constants.CONNECTIONS_KEY).push();
 
     // 1. Participants
-    connRef.child(PARTICIPANTS_KEY).push(userA);
-    connRef.child(PARTICIPANTS_KEY).push(userB);
+    connRef.child(Constants.PARTICIPANTS_KEY).push(userA);
+    connRef.child(Constants.PARTICIPANTS_KEY).push(userB);
     // 2. Data
-    var dataRef = connRef.child(DATA_KEY);
+    var dataRef = connRef.child(Constants.DATA_KEY);
     var quarterRef = dataRef.child(quarter);
     quarterRef.set(combinedWeek);
 
     // Add to users' connections child
-    var usersRef = db.ref(USERS_KEY);
-    var userAConnRef = usersRef.child(userA).child(CONNECTIONS_KEY).push({
-      [CONNECTION_ID_KEY]: connRef.key,
-      [CONNECTION_WITH_KEY]: userB
+    var usersRef = db.ref(Constants.USERS_KEY);
+    var userAConnRef = usersRef.child(userA).child(Constants.CONNECTIONS_KEY).push({
+      [Constants.CONNECTION_ID_KEY]: connRef.key,
+      [Constants.CONNECTION_WITH_KEY]: userB
     });
-    var userBConnRef = usersRef.child(userB).child(CONNECTIONS_KEY).push({
-      [CONNECTION_ID_KEY]: connRef.key,
-      [CONNECTION_WITH_KEY]: userA
+    var userBConnRef = usersRef.child(userB).child(Constants.CONNECTIONS_KEY).push({
+      [Constants.CONNECTION_ID_KEY]: connRef.key,
+      [Constants.CONNECTION_WITH_KEY]: userA
     });
 
     resolve('Connection saved.');
@@ -176,7 +196,7 @@ function connect(db, userA, userB, request, quarterId) {
   // TODO: confirm that a connection doesn't exist
 
   // Download schedule for both users
-  var schedulesRef = db.ref(SCHEDULES_KEY);
+  var schedulesRef = db.ref(Constants.SCHEDULES_KEY);
   var userARef = schedulesRef.child(userA + '/' + quarterId);
   var userBRef = schedulesRef.child(userB + '/' + quarterId);
 
@@ -186,12 +206,12 @@ function connect(db, userA, userB, request, quarterId) {
     userAQuarter = values[0].val();
     userBQuarter = values[1].val();
 
-    var combinedWeek = combineQuartersIntoWeek(userAQuarter, userBQuarter);
+    var combinedWeek = combineQuartersIntoWeek(userAQuarter, userBQuarter, userA, userB);
 
     if (combinedWeek) {
       // TODO: clean up
       return saveConnection(db, combinedWeek, quarterId, userA, userB).then(function() {
-        var reqRef = db.ref(REQUESTS_KEY);
+        var reqRef = db.ref(Constants.REQUESTS_KEY);
         reqRef.child(userA + '/' + request).set(null);
         reqRef.child(userB + '/' + request).set(null);
         return 'Success';
